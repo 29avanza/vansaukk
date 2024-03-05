4fb00b0d966f46d668e653f71b17a534020faeae
29avanza <142554515+29avanza@users.noreply.github.com>
2024-03-05 10:32:24 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="
+    width=device-width, initial-scale=1">
+    <title>Website Galeri Vansa</title>
+    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+<body>
+<nav class="navbar navbar-expand-lg "style="background-color: #d7e3fc">
+  <div class="container">
+    <a class="navbar-brand" href="index.php">Website Galeri Foto Vansa</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse mt-2" id="navbarNavAltMarkup">
+      <div class="navbar-nav me-auto">
+      
+      </div>
+      <a href="register.php" class="btn btn-outline-primary m-1">Daftar</a>
+      <a href="login.php" class="btn btn-outline-success m-1">Masuk</a>
+    </div>
+  </div>
+</nav>
+
+
+  <center><h1>SELAMAT DATANG DI WEB SITE GALERI FOTO VANSA</h1></center>
+
+
+<footer class="d-flex justify-content-center border-top mt-3
+bg-light fixed-bottom">
+  <p>&copy; UKK RPL 2 2024 | Avanza pradipta f.a</p>
+</footer>
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ webdimas.sql
@@ -0,0 +1,257 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 22, 2024 at 02:31 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `webdimas`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `album`
+--
+
+CREATE TABLE `album` (
+  `albumid` int(11) NOT NULL,
+  `namaalbum` varchar(255) NOT NULL,
+  `deskripsi` text NOT NULL,
+  `tanggalbuat` date NOT NULL,
+  `userid` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `album`
+--
+
+INSERT INTO `album` (`albumid`, `namaalbum`, `deskripsi`, `tanggalbuat`, `userid`) VALUES
+(6, 'andaikan', '                                                                          adndahddhjvg\r\n\r\n                                                                                        ', '2024-02-19', 3),
+(8, 'otomotif', 'oyomoyif', '2024-02-19', 3),
+(9, 'kalangan', 'tytytyrtytyttyrty', '2024-02-21', 3),
+(10, 'otomotif', 'yukaku', '2024-02-21', 4);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `foto`
+--
+
+CREATE TABLE `foto` (
+  `fotoid` int(11) NOT NULL,
+  `judulfoto` varchar(255) NOT NULL,
+  `deskripsifoto` text NOT NULL,
+  `tanggalunggah` date NOT NULL,
+  `lokasifile` varchar(255) NOT NULL,
+  `albumid` int(11) NOT NULL,
+  `userid` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `foto`
+--
+
+INSERT INTO `foto` (`fotoid`, `judulfoto`, `deskripsifoto`, `tanggalunggah`, `lokasifile`, `albumid`, `userid`) VALUES
+(4, 'hape', 'hape', '2024-02-21', '38565776-kelelawar.png', 8, 3),
+(5, 'oyikiokiol', 'wererere', '2024-02-21', '690953405-pohon.jpg', 10, 4);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `komentarfoto`
+--
+
+CREATE TABLE `komentarfoto` (
+  `komentarid` int(11) NOT NULL,
+  `fotoid` int(11) NOT NULL,
+  `userid` int(11) NOT NULL,
+  `isikomentar` text NOT NULL,
+  `tanggalkomentar` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `komentarfoto`
+--
+
+INSERT INTO `komentarfoto` (`komentarid`, `fotoid`, `userid`, `isikomentar`, `tanggalkomentar`) VALUES
+(1, 5, 3, 'wewreererer', '2024-02-22'),
+(2, 5, 3, 'haiii broooku', '2024-02-22'),
+(3, 5, 3, 'haiii broooku', '2024-02-22'),
+(4, 4, 3, 'haiii broooku', '2024-02-22'),
+(5, 4, 3, '', '2024-02-22'),
+(6, 4, 3, 'wewreererer', '2024-02-22'),
+(7, 4, 3, '', '2024-02-22'),
+(8, 5, 3, 'wewreererer', '2024-02-22');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `likefoto`
+--
+
+CREATE TABLE `likefoto` (
+  `likeid` int(11) NOT NULL,
+  `fotoid` int(11) NOT NULL,
+  `userid` int(11) NOT NULL,
+  `tanggallike` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `likefoto`
+--
+
+INSERT INTO `likefoto` (`likeid`, `fotoid`, `userid`, `tanggallike`) VALUES
+(84, 4, 3, '2024-02-21'),
+(89, 5, 4, '2024-02-21'),
+(90, 4, 4, '2024-02-21'),
+(91, 5, 3, '2024-02-22');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `userid` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `namalengkap` varchar(255) NOT NULL,
+  `alamat` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`userid`, `username`, `password`, `email`, `namalengkap`, `alamat`) VALUES
+(1, '\r\n  aa', 'aa', 'aa@gmail.com', 'aa', 'aa'),
+(2, '\r\n  admin', '21232f297a57a5a743894a0e4a801fc3', 'admin@gmail.com', 'admin', 'Indonesia'),
+(3, 'admin', '21232f297a57a5a743894a0e4a801fc3', 'admin@gmail.com', 'dimas marta', 'tanggungan rt4  rw 2'),
+(4, 'aa', '4124bc0a9335c27f086f24ba207a4912', 'aa@gmail.com', 'aa', 'aa');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `album`
+--
+ALTER TABLE `album`
+  ADD PRIMARY KEY (`albumid`),
+  ADD KEY `userid` (`userid`);
+
+--
+-- Indexes for table `foto`
+--
+ALTER TABLE `foto`
+  ADD PRIMARY KEY (`fotoid`),
+  ADD KEY `albumid` (`albumid`),
+  ADD KEY `userid` (`userid`);
+
+--
+-- Indexes for table `komentarfoto`
+--
+ALTER TABLE `komentarfoto`
+  ADD PRIMARY KEY (`komentarid`),
+  ADD KEY `fotoid` (`fotoid`),
+  ADD KEY `userid` (`userid`);
+
+--
+-- Indexes for table `likefoto`
+--
+ALTER TABLE `likefoto`
+  ADD PRIMARY KEY (`likeid`),
+  ADD KEY `fotoid` (`fotoid`),
+  ADD KEY `userid` (`userid`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`userid`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `album`
+--
+ALTER TABLE `album`
+  MODIFY `albumid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `foto`
+--
+ALTER TABLE `foto`
+  MODIFY `fotoid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `komentarfoto`
+--
+ALTER TABLE `komentarfoto`
+  MODIFY `komentarid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `likefoto`
+--
+ALTER TABLE `likefoto`
+  MODIFY `likeid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=92;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `userid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `album`
+--
+ALTER TABLE `album`
+  ADD CONSTRAINT `album_ibfk_1` FOREIGN KEY (`userid`) REFERENCES `user` (`userid`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `foto`
+--
+ALTER TABLE `foto`
+  ADD CONSTRAINT `foto_ibfk_1` FOREIGN KEY (`userid`) REFERENCES `user` (`userid`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `foto_ibfk_2` FOREIGN KEY (`albumid`) REFERENCES `album` (`albumid`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `komentarfoto`
+--
+ALTER TABLE `komentarfoto`
+  ADD CONSTRAINT `komentarfoto_ibfk_1` FOREIGN KEY (`userid`) REFERENCES `user` (`userid`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `komentarfoto_ibfk_2` FOREIGN KEY (`fotoid`) REFERENCES `foto` (`fotoid`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `likefoto`
+--
+ALTER TABLE `likefoto`
+  ADD CONSTRAINT `likefoto_ibfk_1` FOREIGN KEY (`userid`) REFERENCES `user` (`userid`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `likefoto_ibfk_2` FOREIGN KEY (`fotoid`) REFERENCES `foto` (`fotoid`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
